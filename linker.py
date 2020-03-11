c1ec253dc059b6b8f5855b37eb113d086b031ec2
DollaR <cooler84@ukr.net>
2020-03-11 21:00:55 +0200

    create linker module

--- linker.py
+++ linker.py
@@ -26,4 +26,22 @@ class Linker:
     def close(self):
         """Saving links before closing program."""
         with open('links.json', 'w', encoding="utf-8") as links_file:
-            json.dump(self.__links, links_file)
+            json.dump(self.__links, links_file, indent='   ')
+
+    def set_link(self, keycode, path):
+        """Set path of wav file to key code in dict links."""
+        self.__links[keycode] = path
+
+    def get_link(self, keycode):
+        """Return path to wav file for keycode from dict links."""
+        return self.__links.get(keycode, None)
+
+    def del_link(self, keycode):
+        """Delete row in dict of wav file."""
+        if keycode in self.__links:
+            del self.__links[keycode]
+
+    def get_all_links(self):
+        """Return all links in string format for view."""
+        links = sorted(['{} - {}'.format(keycode, path) for keycode, path in self.__links.items()])
+        return '\n'.join(links)
