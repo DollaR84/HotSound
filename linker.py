3d29d0a23ae70c3097f58c80283ba6f87067375a
DollaR <cooler84@ukr.net>
2020-03-12 01:34:15 +0200

    add keyMap dict from wx key codes

--- linker.py
+++ linker.py
@@ -9,6 +9,8 @@ Created on 14.02.2020
 
 import json
 
+import wx
+
 
 class Linker:
     """Class link keys with sound files."""
@@ -23,6 +25,11 @@ class Linker:
             with links_file:
                 self.__links = json.load(links_file)
 
+        self.__keyMap = {}
+        for varName in vars(wx):
+            if varName.startswith("WXK_"):
+                self.__keyMap[str(getattr(wx, varName))] = varName[4:]
+
     def close(self):
         """Saving links before closing program."""
         with open('links.json', 'w', encoding="utf-8") as links_file:
@@ -30,18 +37,18 @@ class Linker:
 
     def set_link(self, keycode, path):
         """Set path of wav file to key code in dict links."""
-        self.__links[keycode] = path
+        self.__links[str(keycode)] = path
 
     def get_link(self, keycode):
         """Return path to wav file for keycode from dict links."""
-        return self.__links.get(keycode, None)
+        return self.__links.get(str(keycode), None)
 
     def del_link(self, keycode):
         """Delete row in dict of wav file."""
-        if keycode in self.__links:
-            del self.__links[keycode]
+        if str(keycode) in self.__links:
+            del self.__links[str(keycode)]
 
     def get_all_links(self):
         """Return all links in string format for view."""
-        links = sorted(['{} - {}'.format(keycode, path) for keycode, path in self.__links.items()])
+        links = sorted(['{} - {}'.format(self.__keyMap.get(keycode, chr(int(keycode))), path) for keycode, path in self.__links.items()])
         return '\n'.join(links)
