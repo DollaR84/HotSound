89e0be2c8a9d8d86fdec2fb9d74dea6df708543e
DollaR <cooler84@ukr.net>
2020-03-26 19:44:25 +0200

    create compile script

--- install.cmd
+++ install.cmd
@@ -1,7 +1,14 @@
 pyinstaller -F --noconsole ^
---add-binary api.pyd;. ^
+--add-binary dialogs/dialogs.pyd;dialogs ^
+--add-binary dialogs/options.pyd;dialogs ^
+--add-binary commands.pyd;. ^
+--add-binary drawer.pyd;. ^
 --add-binary menu.pyd;. ^
---add-binary tree.pyd;. ^
 --add-binary version.pyd;. ^
+--add-binary wxdb.pyd;. ^
+--add-binary configs.pyd;. ^
+--add-binary database.pyd;. ^
+--add-binary linker.pyd;. ^
+--add-binary player.pyd;. ^
 --hidden-import wx ^
 main.pyw
